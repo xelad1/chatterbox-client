8df334fbb62e27a601d661f8e045a58a13b4b2d3
xelad1 <xelad1@gmail.com>
2014-11-10 16:33:48 -0800

    Got chat refresh, and display

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -10,6 +10,6 @@ if (!/(&|\?)username=/.test(window.location.search)) {
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "PARSE_APP_ID");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "PARSE_API_KEY");
+  jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
+  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,30 @@
-// YOUR CODE HERE:
+var displayChat = function(results) {
+    for(var i = 0; i < results.length; i ++) {
+      var htmlString = "";
+      htmlString += "<p>"+ results[i].username + ":" + results[i].createdAt + "</p>";
+      $('#chatter').append(htmlString);
+    }
+}
 
+var checkMessages = [];
+
+var diff = function(arr1, arr2, iteratee) {
+  var results = [];
+  var iterateeArr2 = _.map(arr2, iteratee);
+  _.each(arr1, function(x) {
+    if(iterateeArr2.indexOf(iteratee(x)) === -1) {
+      results.push(x);
+    }
+  });
+  return results;
+}
+
+setInterval(function() {
+  $.get('https://api.parse.com/1/classes/chatterbox', function(x){
+    var newMessages = diff(x.results, checkMessages, function(y) {
+      return y.createdAt;
+    });
+    displayChat(newMessages);
+    checkMessages = checkMessages.concat(newMessages);
+  });
+}, 2000);
