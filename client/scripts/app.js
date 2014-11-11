8d20a63a3ceee5377de10a49e35fa827594630bf
xelad1 <xelad1@gmail.com>
2014-11-10 17:34:10 -0800

    fixed refresh

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,12 +1,42 @@
+
+
+
 var displayChat = function(results) {
     for(var i = 0; i < results.length; i ++) {
       var htmlString = "";
-      htmlString += "<p>"+ results[i].username + ":" + results[i].createdAt + "</p>";
+
+      var displayText = results[i].text;
+
+      if(results[i].text === undefined) {
+        displayText = '';
+      }
+
+      htmlString += "<p>"+ results[i].username + ": " +  escapeHTML(displayText) + "</p>";
       $('#chatter').append(htmlString);
     }
 }
 
 var checkMessages = [];
+
+/*
+ & --> &amp;
+ < --> &lt;
+ > --> &gt;
+ " --> &quot;
+ ' --> &#x27;
+ / --> &#x2F;
+ */
+
+var escapeHTML = function(htmlStr) {
+  htmlStr = htmlStr.replace(/\&/g, "&amp");
+  htmlStr = htmlStr.replace(/\</g, "&lt");
+  htmlStr = htmlStr.replace(/\>/g, "&gt");
+  htmlStr = htmlStr.replace(/\"/g, "&quot");
+  htmlStr = htmlStr.replace(/\'/g, "&#x27");
+  htmlStr = htmlStr.replace(/\//g, "&#x2F");
+
+  return htmlStr;
+}
 
 var diff = function(arr1, arr2, iteratee) {
   var results = [];
@@ -20,7 +50,7 @@ var diff = function(arr1, arr2, iteratee) {
 }
 
 setInterval(function() {
-  $.get('https://api.parse.com/1/classes/chatterbox', function(x){
+  $.get('https://api.parse.com/1/classes/chatterbox',{limit: 20, order: "-createdAt"}, function(x){
     var newMessages = diff(x.results, checkMessages, function(y) {
       return y.createdAt;
     });
